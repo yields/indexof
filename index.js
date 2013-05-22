5ed0ec7e4485f7559daefa4dacdfa5436ffe3df1
Amir Abu Shareb <yields@icloud.com>
2013-05-22 09:31:43 +0300

    fix .indexof on ie8, closes #1

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ var indexof = [].indexOf;
 
 /**
  * Get the index of the given `el`.
- * 
+ *
  * @param {Element} el
  * @return {Number}
  */
 
 module.exports = function(el){
   if (!el.parentNode) return -1;
-  var all = el.parentNode.children;
-  return indexof.call(all, el);
+
+  var list = el.parentNode.children
+    , len = list.length;
+
+  if (indexof) return indexof.call(list, el);
+  for (var i = 0; i < len; ++i) {
+    if (el == list[i]) return i;
+  }
+  return -1;
 };
